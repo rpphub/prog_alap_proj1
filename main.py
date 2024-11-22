33f76b077edb65a79793279c2d9e02aec861165c
Kvl61112 <email@pollakrobert.com>
2024-11-22 10:58:09 +0100

    Restruct

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
 import streamlit as st
-import helper
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from view.diagram import Diagram
+from data.data import Data
 
-import view.plot
+df = Data.get_df_from_csv("data/stadat-jov0001-14.1.1.1-hu.csv")
+df = Data.data_manipulation(df) #Tábla forgatás, manupulálás.
+
+# dataFrame - > ndarray, így könnyebben emésztik a függvények
+yArray = df.iloc[:,1].str.replace(' ','').map(int).values.reshape(-1, 1)
+xArray = df.iloc[:,0].map(int).values.reshape(-1, 1)
 
 linreg = LinearRegression()
-dataFrame = helper.get_dfFromCsv("data/stadat-jov0001-14.1.1.1-hu.csv")
-dataFrame = helper.dfManipulation(dataFrame) #Tábla forgatás, manupulálás.
-
-yArray = dataFrame.iloc[:,1].str.replace(' ','').map(int).values.reshape(-1, 1)
-xArray = dataFrame.iloc[:,0].map(int).values.reshape(-1, 1) #xPS.map(int).values.reshape(-1, 1)
-
-
-#x_train,x_test,y_train,y_test = train_test_split(xArray,yArray,test_size=0.2,random_state=0)
 linreg.fit(xArray,yArray)
+
+r2 = r2_score(yArray,linreg.predict(xArray)) # Regression Error score, ez csak úgy érdekességnek van
+print("Lineáris regresszió pontossága:" + str(r2))
 
 # Streamlit View
 # Title
-st.markdown(
-    """
-    <style>
-    .title {
-        text-align: center;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-st.markdown('<h1 class="title">Programozás alapok beadandó feladat.</h1>', unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Programozás alapok beadandó feladat</h1>", unsafe_allow_html=True)
+st.markdown("***")
 
 # Slider
 regVal = st.slider("Év kiválasztása", 2015, 2050, 2024)
-futureXArr = helper.get_futureList(regVal,2023)
+futureXArr = Data.get_future_list(regVal,2023)
 
 # Year / Income calculated value
 col1,col2 = st.columns(2)
@@ -52,24 +41,14 @@ with col2:
   )
 
 # Plot
-fig, ax = view.plot.buildPlot()
-
-'''
-ax.plot(xArray,yArray,"o",color="orange", label = "Tényleges") #Eredeti adatok
-
-ax.plot(xArray,linreg.predict(xArray),"o:",color="magenta",label = "Lineáris regresszió") #Eredeti X tengely adatok mentén Linreg
-ax.plot(futureXArr,linreg.predict(futureXArr),"o:", color = "darkturquoise", label = "Prediktív") #Változtatható X tengely adat menti Linreg
-
-'''
-st.write(fig) #Grafikon streamlit-ba ágyazása
+dg = Diagram()
+dg.build_data_chart(xArray,yArray)
+dg.build_regression_chart(xArray,linreg.predict(xArray))
+dg.build_future_chart(futureXArr,linreg.predict(futureXArr))
+dg.add_legends()
+st.write(dg.fig) #Grafikon streamlit-ba ágyazása
 
 # Data matrix
 with st.expander("Tényleges Adatok"):
-  st.dataframe(dataFrame)
+  st.dataframe(df)
 
-# Regression Error score
-r2 = r2_score(yArray,linreg.predict(xArray))
-print("Linear regression error score:" + str(r2))
-
-if __name__ == "__main__":
-  print("Teszt")
--- view/diagram.py
+++ view/diagram.py
@@ -10,18 +10,19 @@ class Diagram:
     plt.ylabel("mFt / év")
     plt.xlabel("Év")
     plt.grid(linestyle = '--') #Háttér vonalazás
-    
     self.ax.yaxis.set_major_formatter(mpl.ticker.EngFormatter()) #Ez segít Y tengely milliós érték megjelenítésben
-    self.ax.legend(shadow=True,fontsize = "x-small") #Diagram nevek mérete
 
-  def buildDataChart(self,xArray,yArray):
+  def build_data_chart(self,xArray,yArray):
     self.ax.plot(xArray,yArray,"o",color="orange", label = "Tényleges") #Eredeti adatok
     
-  def buildRegressionChart(self,xArray,yArray):
-    self.ax.plot(xArray,yArray,"o",color="orange", label = "Tényleges") #Eredeti adatok
-    
-  def buildFutureChart(self,xArray,yArray):
-    self.ax.plot(xArray,yArray,"o",color="orange", label = "Tényleges") #Eredeti adatok
+  def build_regression_chart(self,xArray,yArrayPredict):
+    self.ax.plot(xArray,yArrayPredict,"o:",color="magenta",label = "Lineáris regresszió") #Eredeti X tengely adatok mentén Linreg
+
+  def build_future_chart(self,future_xArray,future_yArray):
+    self.ax.plot(future_xArray,future_yArray,"o:", color = "darkturquoise", label = "Prediktív") #Változtatható X tengely adat menti Linreg
+
+  def add_legends(self):
+    self.ax.legend(shadow=True,fontsize = "x-small") #Diagram nevek mérete
     
   
     
