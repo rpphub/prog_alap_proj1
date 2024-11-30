b2823216b45d4b5751a51e4e1ced86759da42733
unknown <email@pollakrobert.com>
2024-11-30 21:58:31 +0100

    Final

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ from sklearn.metrics import r2_score
 from view.diagram import Diagram
 from data.data import Data
 
-df = Data.get_df_from_csv("data/stadat-jov0001-14.1.1.1-hu.csv")
-df = Data.data_manipulation(df) #Tábla forgatás, manupulálás.
+############## LEVI - Data / Manipulation ############
+# Táblázat beolvasás és manipuláció
+df_raw = Data.get_df_from_csv("data/stadat-jov0001-14.1.1.1-hu.csv")
+df = Data.data_manipulation(df_raw) #Tábla forgatás, manupulálás.
 
 # dataFrame - > ndarray, így könnyebben emésztik a függvények
 yArray = df.iloc[:,1].str.replace(' ','').map(int).values.reshape(-1, 1)
 xArray = df.iloc[:,0].map(int).values.reshape(-1, 1)
+############## LEVI vége ############
 
+#Linear Regression
 linreg = LinearRegression()
 linreg.fit(xArray,yArray)
-
 r2 = r2_score(yArray,linreg.predict(xArray)) # Regression Error score, ez csak úgy érdekességnek van
 print("Lineáris regresszió pontossága:" + str(r2))
 
+############## Robi - Streamlit ############
 # Streamlit View
 st.set_page_config(
   page_title="Programozás alapok beadandó",
@@ -50,15 +54,19 @@ with col2:
     value=linreg.predict([[int(regVal)]])
   )
 
-# Plot
+############## Milán - Regression / Diagram ############
 dg = Diagram()
 dg.build_data_chart(xArray,yArray)
 dg.build_regression_chart(xArray,linreg.predict(xArray))
 dg.build_future_chart(futureXArr,linreg.predict(futureXArr))
 dg.add_legends()
 st.write(dg.fig) #Grafikon streamlit-ba ágyazása
+############## Milán vége ############
+
 
 # Data matrix
 with st.expander("Tényleges Adatok"):
-  st.dataframe(df)
+  st.dataframe(df_raw)
 
+with st.expander("Formázott Adatok"):
+  st.dataframe(df)
